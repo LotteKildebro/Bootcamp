aa5ea4f79533f5efa39c43278312f1de0ac9864f
LotteKildebro <4514756@RTS-365.dk>
2018-01-22 10:22:49 +0100

    commit

---
+++ operatores.md
@@ -0,0 +1,20 @@
+
+## Operatorer
+
+1. = : tildelings operator, "lig med" kan f.eks. bruges til at sige "Let navn = 'hej';" .. 
+1. == : sammenlignings operator, f.eks. siger vi "'hej' == navn" - er det 'hej' der ligger i navn ..
+1. === : sammenlignings operator, værdi+datatype..
+1. ! : not, den kan vende et udtryk om.
+1. != : denne operator spørg "er den ikke lig med..."
+1. !== : denne operator spørg "er den ikke lig med..." +datatype.
+1. <: større end
+1. >: mindre end
+1. <=: større end og er lig med
+1. >=: større end og er lig med
+1. +: tillægnings operator
+1. -: fratræknings operator
+1. *: gange
+1. /: divider
+1. %: modulus , den ser på to værdier og ser hvor mange gange de den ene værdi kan gå op i den anden værdi.
+1. &&: denne operator ser på 2 værdier og spørg om disse er sande, hvis dette er tilfældet er det true.
+1. ||: denne operator ser på 2 to værdier individuelt, så den første værdi skal være sand, men ikke nødvendigvis den anden.
---
+++ operators.js
@@ -0,0 +1,88 @@
+
+//let hej = 1;
+//let hejsa = 2;
+
+//console.log(hej + hejsa);
+
+
+//let hej = 1;
+//let hejsa = 2;
+
+//console.log(hej - hejsa);
+
+
+//let hej = 1;
+//let hejsa = 2;
+
+//console.log(hej * hejsa);
+
+//let hej = 1;
+//let hejsa = 2;
+
+//console.log(hej / hejsa);
+
+//let hej = 1;
+//let hejsa = 2;
+
+//console.log(hej % hejsa);
+
+//++ og -- Operators...
+
+//let hej = 1;
+
+//hej++;
+//console.log(hej); 
+
+
+//let hej = 1;
+
+//console.log(hej--); 
+
+// ØVELSE 3 ...
+
+//let x = 5;
+//let y = 10;
+//x += y;
+//console.log(x);
+
+//let x = 5;
+//let y = 10;
+//x -= y;
+//console.log(x);
+
+//let x = 5;
+//let y = 10;
+//x *= y;
+//console.log(x);
+
+//let x = 5;
+//let y = 10;
+//x /= y;
+//console.log(x);
+
+//let x = 5;
+//let y = 10;
+//x %= y;
+//console.log(x);
+
+// ØVELSE 4 ....
+
+//let navn = "Emil";
+//let alder = " 26"
+
+//if (navn == "Emil" && alder == "26"){
+//console.log(if bliver udførst) 
+//}else {
+//console.log(else bliver udførst) 
+//}
+
+//if (navn == "Emil" || alder == "26"){
+//console.log(if bliver udførst) 
+//}else {
+//console.log(else bliver udførst) 
+//}
+
+
+
+
+
